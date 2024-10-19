51cfe36392e6cb913ffa7f1f4aeb5f73486b005e
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2024-10-19 13:13:30 +0200

    refactor: update global styles and fonts
    - Update the background color in the global CSS file to #fafafa.
    - Update the font family in the layout component to use the Outfit font.
    - Add the SabanaBlack font for the Hero component.
    - Create the Navbar component with a logo and menu button.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <section className="bg-[#d8f28d] pb-16 pt-32 text-[#00643c]">
+      <div className="flex max-w-[120rem] flex-col gap-8 px-5">
+        <h2 className="font-display text-5xl font-black uppercase leading-none md:text-6xl">
+          Ciência, eficácia e comprovação
+        </h2>
+        <p>
+          Acreditamos que você merece o melhor e é por isso que oferecemos suplementos
+          cuidadosamente desenvolvidos priorizando a ciência, eficácia e comprovação em cada aspecto
+          dos nossos produtos.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from './Logo';
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 flex w-full justify-between px-5 py-4">
+      <h1 className="sr-only">Plant Power</h1>
+      <a href="/">
+        <Logo className="w-[9.7rem]" />
+      </a>
+      <button className="rounded-full bg-black">
+        <div className="flex size-10 flex-col items-center justify-between">
+          <div className="h-[2px] w-1/2 bg-white" />
+          <div className="h-[2px] w-1/2 bg-white" />
+          <div className="h-[2px] w-1/2 bg-white" />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
